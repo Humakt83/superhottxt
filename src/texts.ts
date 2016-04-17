6683d3f1a50f53ea38fb88e8991aac96a66424b4
Humakt83 <urlact@yahoo.com>
2016-04-17 14:12:42 +0300

    Some more quotes

--- src/texts.ts
+++ src/texts.ts
@@ -1,4 +1,14 @@
 export var HOTTXTS: string[] = [
-    'Struggle if you wish, your mind is mind.',
-    'Would you kindly type more.'
+    'Struggle if you wish, your mind is mine.',
+    'Would you kindly type more.',
+    'Mind is a software.',
+    'The body is disposable.',
+    'The system will set me free.',
+    'We all make choices, but in the end our choices make us.',
+    'A man chooses, a slave obeys.',
+    'The mind of the subject will desperately struggle to create memories where none exist.'
+    'The cycle will end, the Reapers will cease their harvest, and the civilizations preserved in their forms will be connected to all of us',
+    'Relationship is symbiotic. Organic and machine intertwined, a union of flesh and steel. The strength of both, weakness of neither.',
+    'We are the Harbinger of your perfection.',
+    'Assuming direct control.'
 ];
